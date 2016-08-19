130d69439b5033e5a5c67c791f34f8f719d6bb81
Артём Баранов <artem_b89@mail.ru>
2016-08-19 16:45:24 +0300

    Initial commit

---
+++ Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+namespace ExtCore.ThemeManager
+{
+    using Infrastructure;
+
+    public class Startup:IExtension
+    {
+        private IConfigurationRoot _configuration;
+        public Startup()
+        {
+        }
+
+        public IEnumerable<KeyValuePair<int, Action<IApplicationBuilder>>> ConfigureActionsByPriorities
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+        public IEnumerable<KeyValuePair<int, Action<IServiceCollection>>> ConfigureServicesActionsByPriorities
+        {
+            get
+            {
+                return new Dictionary<int, Action<IServiceCollection>>()
+                {
+                    [2000] = services =>
+                    {
+                        services.Configure<RazorViewEngineOptions>(options =>
+                        {
+                            options.ViewLocationExpanders.Add(new ThemeLocationExpander(_configuration));
+                        }
+                        );
+                    }
+                };
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return "ExtCore Theme Manager";
+            }
+        }
+
+        public void SetConfigurationRoot(IConfigurationRoot configurationRoot)
+        {
+            this._configuration = configurationRoot;
+        }
+
+        public void SetServiceProvider(IServiceProvider serviceProvider)
+        {
+        }
+    }
+}
---
+++ ThemeLocationExpander.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.Configuration;
+namespace ExtCore.ThemeManager
+{
+    public class ThemeLocationExpander: IViewLocationExpander
+    {
+        private IConfigurationRoot _configuration;
+        private const string THEME_KEY = "THEME_KEY";
+        public ThemeLocationExpander(IConfigurationRoot configurationRoot)
+        {
+            this._configuration = configurationRoot;
+        }
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            string theme = null;
+            if (context.Values.TryGetValue(THEME_KEY, out theme))
+            {
+                IEnumerable<string> themeLocations = new[]
+                {
+                $"/Themes/{theme}/{{1}}/{{0}}.cshtml",
+                $"/Themes/{theme}/Shared/{{0}}.cshtml"
+            };
+                viewLocations = themeLocations.Concat(viewLocations);
+            }
+            foreach (var el in viewLocations) System.Console.WriteLine(el);
+            return viewLocations;
+        }
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+            context.Values[THEME_KEY] = _configuration["ThemeManager:Theme"];
+        }
+    }
+}
